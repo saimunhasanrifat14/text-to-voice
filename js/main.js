493d0088076e0d5add25c9946480728367f21b1e
Saimun Hasan <saimunhasanrifat14@gmail.com>
2025-01-27 22:47:02 +0600

    complate functionel part

--- js/main.js
+++ js/main.js
@@ -1,10 +1,22 @@
-let speech = new SpeechSynthesisUtterance();
 
 let input = document.querySelector(".input")
 let type = document.querySelector(".type")
 let listen = document.querySelector(".listen")
 
-listen.addEventListener("click", () =>{
+let speech = new SpeechSynthesisUtterance();
+let voices = [];
+
+window.speechSynthesis.onvoiceschanged = () => {
+    voices = window.speechSynthesis.getVoices();
+    speech.voice = voices[0];
+
+    voices.forEach((voice, i) => (type.options[i] = new Option(voice.name, i)));
+}
+type.addEventListener("change", ()=>{
+    speech.voice = voices[type.value];
+})
+
+listen.addEventListener("click", () => {
     speech.text = input.value;
     window.speechSynthesis.speak(speech)
-})
+});
